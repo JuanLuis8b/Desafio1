16a3154b3d14bb303839184af36f3e6f2a746da6
Juan Luis <j.becerra@udea.edu.co>
2024-09-14 18:46:39 -0500

    Avance 1: Creación de la estrucutra de datos para la recopilación e intento de organizar las estructuras de control

--- main.cpp
+++ main.cpp
@@ -4,30 +4,76 @@
 
 Adafruit_LiquidCrystal lcd_1(0);
 
+//funciones de apoyo
+void copy ();
+
 //funciones
 void redimensionar();
 int frecuencia();
 int amplitud();
 void tipo ();
+bool presionado();
 
 //variables para leer los datos del generador
 int analogPin = A0;
-int dato = 0;
-
-//variables para los pulsadores
 int pulsador1 = 2;
 int pulsador2 = 4;
 
+//variables globlales
+int dato = 0;
+int capDatos = 1000;
+int capCiclos = 3;
+
+int **ciclos = new int*[capCiclos];
+for (unsigned int i = 0; i < capCiclos; i++){
+    ciclos[i] = new int [capDatos]; }
+
+int numelem=0;
+
+//setup
 void setup() {
   Serial.begin(9600);
   lcd_1.begin(16, 2);
   pinMode (pulsador1, INPUT);
   pinMode (pulsador2, INPUT);
+
 }
 
+//loop
 void loop(){
-  if (digitalRead(pulsador1) == HIGH){
-    dato = analogRead(analogPin);
-    Serial.println(dato);
+
+ dato = analogRead(analogPin);
+
+//implementacion de funciones
+
+void redimensionar(int**&arr , int &capacidadC, int &capacidadF){
+
+  unsigned int nuevaCapC = capacidadC*2;
+  unsigned int nuevaCapF = capacidadF*2;
+
+  int**nuevoArr = new int *[nuevaCapF];
+  for (unsigned int i = 0; i < nuevaCapF;i++){
+    nuevoArr[i] = new int [nuevaCapC];
   }
+
+  for (unsigned int i = 0; i < capacidadF; i++) {
+     for (unsigned int j = 0; j < capacidadC; j++) {
+         nuevoArr[i][j] = arr[i][j];
+     }
+  }
+
+  for (unsigned int i = 0; i < capacidadF;i++){
+    delete[] arr[i];
+  }
+  delete [] arr;
+
+  arr = nuevoArr
+  capacidadC = nuevaCapC;
+  capacidadF = nuevaCapF;
+}
+
+bool presionado(analogPin){
+
+
+
 }
